3cff82de1cab2719d006a3bbc97d44913fe8a51b
Caleb <calebcowen@gmail.com>
2017-06-04 11:44:20 -0600

    Finish template

--- app/models/section_image_text_widget.rb
+++ app/models/section_image_text_widget.rb
@@ -2,19 +2,14 @@ class SectionImageTextWidget < Widget
   attribute :text, :html
   attribute :image, :reference
   attribute :text_color, :html
-  attribute :text_position, :enum, values: ['top','center','bottom'], default: 'top'
+  attribute :text_position, :enum, values: ['top','middle','bottom'], default: 'top'
 
   def column_size(image)
     self.container.column_size(image)
   end
 
   def section_style
-    styles = "background: url(#{image.binary_url}) top center 0 no-repeat; z-index: -1; background-size: cover;" if image.present?
-    return styles
-  end
-
-  def position_class
-    "scrivito-t-image-position-#{image_position}"
+    "background: url(#{image.binary_url}) top center 0 no-repeat; z-index: 0; background-size: cover;" if image.present?
   end
 
   def text_position_class
